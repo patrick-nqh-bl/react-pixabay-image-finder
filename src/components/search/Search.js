9baefb68d010642fdc7b1656dc7466470f748ac7
Patrick <patrick204nqh@gmail.com>
2020-05-13 11:32:10 +0700

    add ImageResults component and handle open, close Modal

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -15,13 +15,18 @@ class Search extends Component {
   };
 
   onTextChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value }, () => {
-      axios
-        .get(
-          `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`
-        )
-        .then((res) => this.setState({ images: res.data.hits }))
-        .catch((err) => console.log(err));
+    const val = e.target.value;
+    this.setState({ [e.target.name]: val }, () => {
+      if (val === '') {
+        this.setState({ images: [] });
+      } else {
+        axios
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+          )
+          .then((res) => this.setState({ images: res.data.hits }))
+          .catch((err) => console.log(err));
+      }
     });
   };
 
@@ -53,7 +58,7 @@ class Search extends Component {
         </SelectField>
         <br />
         {this.state.images.length > 0 ? (
-          <ImageResults image={this.state.images} />
+          <ImageResults images={this.state.images} />
         ) : null}
       </>
     );
