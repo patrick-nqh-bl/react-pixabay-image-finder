f47074ee49f1bb8dff4dfa3ab23c2cd944bd1dbd
Patrick <patrick204nqh@gmail.com>
2020-05-13 10:22:41 +0700

    add Search component and hadle get image API

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Patrick's Project (using React, Material UI, PixaBay API)
+
+1. Re-code by me
+
+- Source video:
+
+> https://www.youtube.com/watch?v=dzOrUmK4Qyw
+
+##### Thank to Traversy Media Youtube channel
--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Navbar from './components/navbar/Navbar';
+import Search from './components/search/Search';
+
 import './App.css';
 
 function App() {
-  return <div className='App'>Hello</div>;
+  return (
+    <MuiThemeProvider>
+      <>
+        <Navbar />
+        <Search />
+      </>
+    </MuiThemeProvider>
+  );
 }
 
 export default App;
